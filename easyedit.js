29cfdcc1e02d71149e6ea9798f8d6d26945c4b44
TgmStudio <tgmstudio@outlook.jp>
2024-12-19 19:02:42 +0900

    Add files via upload

---
+++ easyedit.js
@@ -0,0 +1,24 @@
+const queryString = location.search;
+const params = new URLSearchParams(queryString);
+const maintxt = document.getElementsByTagName("h3");
+var button = document.getElementById("parent");
+console.log(params.get('a'));
+console.log(params.get('b'));
+document.querySelector('.section').innerHTML = ('<dd id="ha"></dd>');
+document.querySelector('.name').innerHTML = ('<dd id="hb"></dd>');
+document.querySelector('#ha').textContent = (params.get('a'));
+document.querySelector('#hb').textContent = (params.get('b'));
+for (let i = 0; i < maintxt.length; i++) {
+if (maintxt[i].innerHTML === '<ruby>先生<rt>せんせい</rt></ruby>からの<ruby>指示<rt> しじ</rt></ruby>') {
+    maintxt[i].innerHTML = 'EasyEdit';
+}
+}
+document.querySelector('.label').textContent = ('');
+document.querySelector('.discription').textContent = ('EasyEditをご利用いただきありがとうございます。');
+for (let i = 0; i < maintxt.length; i++) {
+if (maintxt[i].innerHTML === '<ruby>自由<rt>じゆう</rt></ruby><ruby>学習<rt>がくしゅう</rt></ruby>') {
+    maintxt[i].innerHTML = (params.get('c'));
+}
+}
+document.querySelector('[onclick="pageJumpToQuestion(4, 'MTDAED1214', 'KDZD06048', 6)]').textContent = (params.get('d'));
+document.querySelector('[onclick="pageJump('https://ela.education.ne.jp/students/free');"]').textContent = (params.get('e'));
